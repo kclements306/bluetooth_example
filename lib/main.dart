87c9f2198ffe0026b2b133d1858858a9af40ca27
Ken Clements <>
2021-03-22 07:36:32 -0600

    Replaced deprecated code

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_blue_example/widgets.dart';
+import 'package:connectivity/connectivity.dart';
 
 void main() {
   runApp(FlutterBlueApp());
